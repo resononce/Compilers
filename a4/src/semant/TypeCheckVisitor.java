32e3914a2a6ec558f453cd68572e335365008d1c
Resononce <resononcefreq@gmail.com>
2019-11-19 20:51:56 -0600

    fixed array kinda

--- a4/src/semant/TypeCheckVisitor.java
+++ a4/src/semant/TypeCheckVisitor.java
@@ -2203,8 +2203,8 @@ public class TypeCheckVisitor extends SemanticVisitor {
                 return "Object";
             }
             else {
-                node.setExprType(varType);
-                return varType;
+                node.setExprType(varType.replace("[]", ""));
+                return varType.replace("[]", "");
             }
         }
         node.setExprType("Object");
